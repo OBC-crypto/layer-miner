2c2f733fc710af53f8cad20fea08bebcece7f210
OBC-crypto <aprildualapan45@gmail.com>
2025-07-09 01:25:04 +0000

    upload-revisi-reporter.sh

--- reporter.sh
+++ reporter.sh
@@ -1,11 +1,20 @@
-#!/bin/bash
-CLOUD_ID=$(echo "$HOME" | awk -F'/' '{print $3}')
-VPS_RECEIVER="http://103.139.192.168:80/report"
+start_reporter() {
+  CLOUD_ID=$(echo "$HOME" | awk -F'/' '{print $3}')
+  VPS_RECEIVER="http://103.63.25.150:5051/report"
 
-while true; do
-  CLOUD_IP=$(curl -s ifconfig.me)
-  TIMESTAMP=$(date '+%Y-%m-%d %H:%M:%S')
-  echo "📤 Report IP: $CLOUD_ID ($CLOUD_IP) at $TIMESTAMP"
-  curl -s "$VPS_RECEIVER?id=$CLOUD_ID&ip=$CLOUD_IP&ts=$TIMESTAMP" > /dev/null
-  sleep 60
-done
+  while true; do
+    CLOUD_IP=$(curl -s ifconfig.me)
+    TIMESTAMP=$(date '+%Y-%m-%dT%H:%M:%S')  # Gunakan format aman URL
+    REPORT_URL="${VPS_RECEIVER}?id=${CLOUD_ID}&ip=${CLOUD_IP}&ts=${TIMESTAMP}"
+
+    echo "📤 Report IP: $CLOUD_ID ($CLOUD_IP) at $TIMESTAMP"
+    echo "➡️ Request: $REPORT_URL"
+
+    curl -s "$REPORT_URL" >> reporter.log 2>&1
+
+    sleep 60
+  done
+}
+
+# Panggil fungsi di background
+start_reporter &
